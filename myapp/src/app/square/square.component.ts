3d911414001814db905a6b9edab2a1abd3d00b4b
Vishal Palkar <palkarv03@gmail.com>
2020-08-06 12:40:21 +0530

    play game

---
+++ myapp/src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      <button>{{ value }}</button>
+    </p>
+  `,
+  styles: [],
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
+}
